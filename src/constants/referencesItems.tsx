d097e549d53aeada16e64d951210b6f8b31cde47
Tomasz Ż. <tzuber95@gmail.com>
2025-04-07 17:15:10 +0200

    Fix some small typos in readme

--- src/constants/referencesItems.tsx
+++ src/constants/referencesItems.tsx
@@ -3,9 +3,9 @@ import References2 from '../../public/photos/ref2.jpg'
 import References3 from '../../public/photos/ref3.jpg'
 
 const referencesItems = [
-    { photo: References1, alt: 'List referencyjny od firmy X' },
+    { photo: References1, alt: 'List referencyjny od firmy Z' },
     { photo: References2, alt: 'List referencyjny od firmy Y' },
-    { photo: References3, alt: 'List referencyjny od firmy Z' },
+    { photo: References3, alt: 'List referencyjny od firmy X' },
 ]
 
 export default referencesItems
